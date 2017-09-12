64ef930808d400ad24a421b134534ae91e422655
nktamars <fitisovn@meta.ua>
2017-09-13 00:24:29 +0300

    mucho bene

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,27 +13,6 @@ function closeNav() {
 
 
 
-///////////////////////LOADMORE////////////////////
-
-$(document).ready(function () {
-    $('.gallery li:lt(3)').show();
-    $('.less').hide();
-    var items =  6;
-    var shown =  3;
-    $('.more').click(function () {
-        $('.less').show();
-        shown = $('.gallery li:visible').size()+3;
-        if(shown< items) {$('.gallery li:lt('+shown+')').show(300);}
-        else {$('.gallery li:lt('+items+')').show(300);
-             $('.more').hide();
-             }
-    });
-    $('.less').click(function () {
-        $('.gallery li').not(':lt(3)').hide(300);
-        $('.more').show();
-        $('.less').hide();
-    });
-});
 
 /////////////////////////LINK///////////////////////////
 
